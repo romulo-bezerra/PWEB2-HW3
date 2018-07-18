18f09f87825f0504bca199a3d8ba5f2f85664097
romulo-soares <bromulosoares@gmail.com>
2018-07-18 10:22:57 -0300

    Gateway com Zull -|ADD

--- supermercado/src/main/java/br/edu/ifpb/supermercado/SupermercadoApplication.java
+++ supermercado/src/main/java/br/edu/ifpb/supermercado/SupermercadoApplication.java
@@ -3,7 +3,9 @@ package br.edu.ifpb.supermercado;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SupermercadoApplication {
